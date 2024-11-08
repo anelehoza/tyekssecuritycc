d0e8fa834b238b7fc4a683d8b72a7296f9c4fe96
anele376@gmail.com <anele376@gmail.com>
2024-11-08 17:04:25 +0200

    Updates

--- public/js/index.js
+++ public/js/index.js
@@ -1,155 +1,204 @@
 //mobile navigation
 
-window.onload = function(){
-const navMenu = document.getElementById('nav-menu')
-const navToggle = document.getElementById('nav-toggle')
-const navClose = document.getElementById('nav-close')
-const menu_button = document.getElementById('menu-button')
-const menu_close = document.getElementById('menu-close')
- 
-let show_menu = false
+window.onload = function () {
+  const navMenu = document.getElementById("nav-menu");
+  const navToggle = document.getElementById("nav-toggle");
+  const navClose = document.getElementById("nav-close");
+  const menu_button = document.getElementById("menu-button");
+  const menu_close = document.getElementById("menu-close");
 
-menu_button.addEventListener('click',toggle)
+  let show_menu = false;
 
-function toggle(){
-if(!show_menu){
-  navMenu.classList.add('show')
-  menu_button.classList.add('close')
+  menu_button.addEventListener("click", toggle);
 
-  //set menu state
-  show_menu = true
-}else {
-  menu_button.classList.remove('close')
-  navMenu.classList.remove('show')
-   
-  //set menu state
-  show_menu = false
-}
-}
-}
+  function toggle() {
+    if (!show_menu) {
+      navMenu.classList.add("show");
+      menu_button.classList.add("close");
 
+      //set menu state
+      show_menu = true;
+    } else {
+      menu_button.classList.remove("close");
+      navMenu.classList.remove("show");
 
-$(document).ready(function(){
-  $(window).on('load scroll',function(){
-      $('.navbar').removeClass('nav-toggle');
-      if($(window).scrollTop()>35)
-      {
-          $('.header').css({'background':'#009e60','box-shadow':'0 .2rem .2rem rgba(0,0,0,.4) '});
-      }
-      else
-      {
-          $('.header').css({'background':'none','box-shadow':'none'});
-      }
-      
+      //set menu state
+      show_menu = false;
+    }
+  }
+};
+
+$(document).ready(function () {
+  $(window).on("load scroll", function () {
+    $(".navbar").removeClass("nav-toggle");
+    if ($(window).scrollTop() > 35) {
+      $(".header").css({
+        background: "#009e60",
+        "box-shadow": "0 .2rem .2rem rgba(0,0,0,.4) ",
+      });
+    } else {
+      $(".header").css({ background: "none", "box-shadow": "none" });
+    }
   });
-//Back to top
+  //Back to top
   $(window).scroll(function () {
-   if ($(this).scrollTop() > 100) {
-       $('.back-to-top').fadeIn('slow');
-   } else {
-       $('.back-to-top').fadeOut('slow');
-   }
- });
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
 
- $('.back-to-top').click(function () {
-   $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-   return false;
- });
- 
- });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+});
 
- //Home section image slider
+//Home section image slider
 
- let homeIndex = 0
- showHomeSlides()
+let homeIndex = 0;
+showHomeSlides();
 
- function showHomeSlides() {
-  let i 
-  let homeSlides = document.getElementsByClassName("")
+function showHomeSlides() {
+  let i;
+  let homeSlides = document.getElementsByClassName("");
   for (i = 0; i < homeSlides.length; i++) {
-    homeSlides[i].style.display ="none"
+    homeSlides[i].style.display = "none";
   }
-  homeSlides++
+  homeSlides++;
   if (homeIndex > homeSlides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }
-  homeSlides[homeIndex-1].style.display = 0
-  setTimeout(showHomeSlides, 3000)
- }
-
-
-
+  homeSlides[homeIndex - 1].style.display = 0;
+  setTimeout(showHomeSlides, 3000);
+}
 
 // Open Modal
 function openModal() {
-   document.getElementById("myModal").style.display = "block"
+  document.getElementById("myModal").style.display = "block";
 }
 
 // span close
-function closeModal(){
-  document.getElementById("myModal").style.display = "none"
-  
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
 }
- 
+
 //Modal slider image
 
-var slideIndex = 1
-showSlides(slideIndex)
+var slideIndex = 1;
+showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n)
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n)
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
-var slides = document.getElementsByClassName("mySlides")
- if (n > slides.length) {slideIndex = 1}
- if (n < 1){slideIndex = slides.length}
-  for(i = 0; i < slides.length; i++){
-     slides[i].style.display = "none"
-    }
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block"
-  slides[slideIndex-1].style.display = "block"
-  
- }
- 
- //partners slides
-
- (function(){
-  var swiper = new Swiper('clients-containe',{
-    spaceBetween: 30,
-    effect: 'fade',
-    autoplay: true, 
-  })
- })(jQuery)
-
- //New Page Section
-
- // Create Post to server
-//  function myArticle(e) {
-//   e.preventDefualt()
-//   var title = document.getElementById('title').value
-//   var author = document.getElementById('author').value
-//   var content = document.getElementById('content').value
-//   var category = document.getElementById('category').value
-//  }
-
- function newsUser(e) {
-  e.preventDefualt()
-  var username = document.getElementById('username').value
-  var password = document.getElementById('password').value
-  var email = document.getElementById('email').value
-
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
+//partners slides
 
-//Deleting Article
+(function () {
+  var swiper = new Swiper("clients-containe", {
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: true,
+  });
+})(jQuery);
+
+//New Page Section
+
+//Create Post to server
+function myArticle(e) {
+  e.preventDefualt();
+  var title = document.getElementById("title").value;
+  var author = document.getElementById("author").value;
+  var content = document.getElementById("content").value;
+  var category = document.getElementById("category").value;
+}
+function myJobs(e) {
+  e.preventDefualt();
+  var title = document.getElementById("title").value;
+  var author = document.getElementById("author").value;
+  var content = document.getElementById("content").value;
+  var category = document.getElementById("category").value;
+}
+
+// function newsUser(e) {
+//   e.preventDefualt();
+//   var username = document.getElementById("username").value;
+//   var password = document.getElementById("password").value;
+//   var email = document.getElementById("email").value;
+// }
+
+// ?
+$('article').readmore();
+$('article').readmore({ speed: 75, lessLink: '<a href="#">Read less</a>' });
 
 
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+  .then(response => {
+    if(!response) {
+      throw new Error('network is not ok')
+    }
+    return response.json()
+  })
+  .then(data =>{
+    console.log(data)
+    displayData(data)
+  })
+  .catch ((error) => console.log('Error:', error))
 
- 
+function displayData(data) {
+  const cocktail = data.drinks[0]
+  const cocktailDiv = document.getElementById('cocktail')
+
+  const cocktailName = cocktail.strDrink
+  const heading = document.createElement('h1')
+  heading.innerHTML = cocktailName
+  cocktailDiv.appendChild(heading)
+
+  const cocktailImg = document.createElement("img");
+  cocktailImg.src = cocktail.strDrinkThumb;
+  cocktailDiv.appendChild(cocktailImg);
+  document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+
+  const cocktailIngredients = document.createElement("ul");
+  cocktailDiv.appendChild(cocktailIngredients);
+  const getIngredients = Object.keys(cocktail)
+    .filter(function (ingredient) {
+      return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+      if (cocktail[ingredient] != null) {
+        ingredients[ingredient] = cocktail[ingredient];
+      }
+      return ingredients;
+    }, {});
+  for (let key in getIngredients) {
+    let value = getIngredients[key];
+    listItem = document.createElement("li");
+    listItem.innerHTML = value;
+    cocktailIngredients.appendChild(listItem);
+}
+}
--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -7,110 +7,53 @@ const multer = require('multer')
 
 
 var storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null,"./uploads")
-    },
+    destination: "/public/uploads",
     filename: function(req, file, cb){
-        cb(null, file.filename+ "_" + Date.now() + "_"+ file.originalname)
+        cb(null,file.filename + "_" + Date.now())
     }
 })
-
-var upload = multer({storage: storage
-}).single("image")
+const upload = multer({storage: storage})
 //Get All articles
 
-router.get('/news', async (req, res) => {
-    try {
-        const articles = await Article.find()
-        const image = path.join(__dirname + '/uploads')
-        res.render('news', { articles, error: null })
-    } catch (err) {
-        console.error('Sorry no articles available', err)
-        res.status(500).render( 'news', {error: 'Sorry no posts available', articles: []})
-    }
-})
-
-  router.get('/createPost', async (req, res) => {
-      try {
-          res.render('createPost')
-      } catch (err) {
-          console.error('Sorry no articles available', err)
-          res.status(500).render( 'createPost', {error: 'Sorry no posts available', articles: []})
-      }
-  })
-
-//Create article
-  router.post('/createPost', upload, async (req, res) => {
-      try {
-                   const article = new Article ({
-                       title: req.body.title,
-                       author: req.body.author,
-                        content: req.body.content,
-                       category: req.body.category,
-                       image: req.file.filename
-                   })
-                    await article.save()
-                    
-                   res.status(201).send('Succefully created')
-               } catch (err) {
-                   res.status(400).json({
-                       message: err.message
-                   })
-               }
- })
-// Get single post
-//  router.get('/:id', async (req, res) => {
-//     try {
-//         const article = await Article.findById(req.params.id);
-//         if (article == null) {
-//             return res.status(404).json({
-//                 message: 'Article not found'
-//             });
-//         }
-//         res.json(article)
-//     } catch (err) {
-//         return res.status(500).json({
-//             message: err.message
-//         });
-//     }
-// });
-
-
-// //Update article
-//  router.put('/:id', async (req, res) =>{
-//      try {
-//          const article = await Article.findByIdAndUpdate(
-//              req.params.id, req.body, {new: true})
-//          if (!article) {
-//              return res.status(404).json({
-//                  message: 'Article not found'
-//              })
-//          }
-//          res.json({ message: 'Article updated successfully '})
-//      } catch (err) {
-//          res.status(500).json({
-//              message : err.message
-//          })
-//      }
-//  })
-
-//  //Delete article
-  router.delete('/news:id', async (req, res) =>{
+ router.get('/news', async (req, res) => {
      try {
-         const article = await Article.findByIdAndDelete(
-             req.params.id)
-         if (!article) {
-             return res.status(404).json({
-                 message: 'Article not found'
-             })
-         }
-         res.json({ message: 'Article Deleted Successfully '})
+         const articles = await Article.find().sort('-createdAt').exec()
+         res.render('news', { articles, error: null })
      } catch (err) {
-         res.status(500).json({
-             message : err.message
-         })
+         console.error('Sorry no articles available', err)
+         res.status(500).render( 'news', {error: 'Sorry no posts available', articles: []})
      }
  })
 
 
+// router.post('/createPost', upload.single('image'), async (req, res) => {
+//     try {
+//                  const article = new Article ({
+//                      title: req.body.title,
+//                      author: req.body.author,
+//                       content: req.body.content,
+//                      category: req.body.category,
+//                      image: req.file.image,
+//                  })
+//                   await article.save()
+//                 // res.status(201).send('Succefully created')
+//                  res.redirect('blog')
+
+//              } catch (err) {
+//                  res.status(400).json({
+//                      message: err.message
+//                  })
+//              }
+// })
+
+
+router.get('/image/:id',(req,res)=>{
+    image.findById(req.params.id,(err,data)=>{
+        if(!err){
+            res.contentType(data.img.contentType);
+            res.send(data.img.data);
+        }
+    });
+
+});
 module.exports = router
