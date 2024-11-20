3ff4c87f203661ae70cfb0f2024e222e2cd7a336
anele376@gmail.com <anele376@gmail.com>
2024-11-20 10:59:14 +0200

    updated package json file

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,4 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
-const browserSync = require('browser-sync')
 
- gulp.task('sass', function(){
-     return gulp.src(['dist/scss/*.scss'])
-     .pipe(sass())
-     .pipe(gulp.dest('dist/css'))
-     .pipe(browserSync.stream())
- })
-
- //watch & serv
- gulp.task('serve',['sass'], function(){
-     browserSync.init({
-         server:'./dist'
-     })
-
-     gulp.watch(['dist/scss/*.scss'], ['sass']);
-     gulp.watch(['dist/*html']).on('change', browserSync.reload)
- })
 
  gulp.task('default', ['serve'])
