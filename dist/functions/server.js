1f3013e4d71ec27454879ad19adaba62aed9630c
anele376@gmail.com <anele376@gmail.com>
2024-11-21 12:24:45 +0200

    added netlify toml build file

--- dist/functions/server.js
+++ dist/functions/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const serverless = require('serverless-http')
 
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -119,3 +120,9 @@ app.get("/careers", function (req, res) {
 app.listen(3000, (req, res) => {
   console.log(`Server running on..`);
 });
+
+
+app.use('/.netlify/functions/server', router)
+
+module.exports = app
+module.handler = serverless(app)
